4ce7c12c7f726b49f5fc6c953ee1994411bd79e0
Stephen <pham.vu@outlook.com>
2021-07-02 18:16:52 +0700

    Stephen solved level-8

--- board.rb
+++ board.rb
@@ -55,8 +55,8 @@ class Board
     rows.all? { |row| solved_set?(row) } &&
       columns.all? { |col| solved_set?(col) } &&
       squares.all? { |square| solved_set?(square) }
-    end
   end
+  
 
   def solved_set?(tiles)
     nums = tiles.map(&:value)
