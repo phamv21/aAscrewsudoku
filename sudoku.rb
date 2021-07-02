90a01c10ee0c29a76db85e15a30e0ccfc11ec857
Stephen <pham.vu@outlook.com>
2021-07-02 17:35:52 +0700

    Stephen_solved_lvl_3

--- sudoku.rb
+++ sudoku.rb
@@ -12,22 +12,22 @@ class SudokuGame
 
   def get_pos
     pos = nil
-    until pos && valid_val?(pos)
+    until pos && valid_pos?(pos)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
       print "> "
-      pos = parse_val(gets.chomp)
+      pos = parse_pos(gets.chomp)
     end
     pos
   end
 
   def get_val
     val = nil
-    until val && valid_pos?(val)
+    until val && valid_val?(val)
       puts "Please enter a position on the board (e.g., '3,4')"
       print "> "
 
       begin
-        val = parse_pos(gets.chomp)
+        val = parse_val(gets.chomp)
       rescue
         puts "Invalid position entered (did you use a comma?)"
         puts ""
@@ -48,9 +48,10 @@ class SudokuGame
 
   def play_turn
     board.render
-
-    val = get_val
+    
     pos = get_pos
+    val = get_val
+    
 
     board[pos] = val
   end
